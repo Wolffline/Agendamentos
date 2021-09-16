5d7704442c4c174743a319b5c106db9fedfce726
Wolffline <alineesilva101@gmail.com>
2021-09-15 21:47:04 -0300

    Primeiro projetinho :)

---
+++ controller/ControllerCadastro.php
@@ -0,0 +1,33 @@
+<?php
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+require_once("$root/Agendamentos/model/cadastro.php");
+
+class ControllerCadastro{
+
+    private $cadastro;
+
+    public function __construct(){
+        $this->cadastro = new Cadastro();
+        if(isset($_GET['funcao']) && $_GET['funcao'] == "cadastro"){
+            $this->incluir();
+        }else if(isset($_GET['funcao']) && $_GET['funcao'] == "editar"){
+            $this->editar($_GET['id']);
+        }
+    }
+
+    private function incluir(){
+        $this->cadastro->setNome($_POST['txtNome']);
+        $this->cadastro->setTelefone($_POST['txtTelefone']);
+        $this->cadastro->setOrigem($_POST['txtOrigem']);
+        $this->cadastro->setData_contato(date('Y-m-d',strtotime($_POST['txtDataContato'])));
+        $this->cadastro->setObservacao($_POST['txtObservacao']);
+        $result = $this->cadastro->incluir();
+        if($result >= 1){
+            echo "<script>alert('Registro incluído com sucesso!');document.location='../index.php'</script>";
+        }else{
+            echo "<script>alert('Erro ao gravar registro!');</script>";
+        }
+    }
+}
+new ControllerCadastro();
+?>
